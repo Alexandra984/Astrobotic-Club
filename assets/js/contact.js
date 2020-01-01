bfa57b9ec42539946de32f2614d8f45d87fe5bca
Alexandra984 <alexandragrecu984@yahoo.com>
2020-01-02 00:18:17 +0200

    Create validateForm function

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -18,10 +18,10 @@ const createRegisterPage = ()=> {
         input1.setAttribute("type", "text");
         input1.setAttribute("placeholder", "First Name");
         input1.setAttribute("aria-describedby", "error-firstName");
+        input1.setAttribute("id", "firstName");
         document.getElementById("form-1").appendChild(input1);
         const div1 = document.createElement("DIV");
         div1.setAttribute("id", "error-firstName");
-        div1.innerHTML = "buna";
         document.getElementById("form-1").appendChild(div1);
         
 
@@ -29,11 +29,11 @@ const createRegisterPage = ()=> {
         const input2 = document.createElement("INPUT");
         input2.setAttribute("type", "text");
         input2.setAttribute("placeholder", "Last Name");
-        input1.setAttribute("aria-describedby", "error-lastName");
+        input2.setAttribute("aria-describedby", "error-lastName");
+        input2.setAttribute("id", "lastName");
         document.getElementById("form-1").appendChild(input2);
         const div2 = document.createElement("DIV");
         div2.setAttribute("id", "error-lastName");
-        div2.innerHTML = "buna";
         document.getElementById("form-1").appendChild(div2);
         
 
@@ -41,11 +41,11 @@ const createRegisterPage = ()=> {
         const input3 = document.createElement("INPUT"); 
         input3.setAttribute("type", "email");
         input3.setAttribute("placeholder", "Email");
-        input1.setAttribute("aria-describedby", "error-email");
+        input3.setAttribute("aria-describedby", "error-email");
+        input3.setAttribute("id", "email");
         document.getElementById("form-1").appendChild(input3);
         const div3 = document.createElement("DIV");
         div3.setAttribute("id", "error-email");
-        div3.innerHTML = "buna";
         document.getElementById("form-1").appendChild(div3);
         
 
@@ -53,11 +53,18 @@ const createRegisterPage = ()=> {
         const input4 = document.createElement("INPUT");
         input4.setAttribute("type", "password");
         input4.setAttribute("placeholder", "Password");
-        input1.setAttribute("aria-describedby", "error-password");
+        input4.setAttribute("aria-describedby", "error-password");
+        input4.setAttribute("id", "password");
         document.getElementById("form-1").appendChild(input4);
+        
+        const i = document.createElement("i");
+        i.setAttribute("id", "pass-status");
+        i.setAttribute("class", "fa fa-eye");
+        i.setAttribute("aria-hidden", "true");
+        document.getElementById("form-1").appendChild(i);
+
         const div4 = document.createElement("DIV");
         div4.setAttribute("id", "error-password");
-        div4.innerHTML = "buna";
         document.getElementById("form-1").appendChild(div4);
         
 
@@ -168,6 +175,8 @@ const createRegisterPage = ()=> {
         
         // button input
         const buton1 = document.createElement("BUTTON");
+        buton1.setAttribute("type", "submit");
+        buton1.setAttribute("id", "buton");
         const text1 = document.createTextNode("Trimite");
         buton1.appendChild(text1);
         document.getElementById("form-1").appendChild(buton1);
@@ -175,5 +184,98 @@ const createRegisterPage = ()=> {
     };
 
     createForm();
+
+    // Create the validation-form function
+    const validateForm = () => {
+        
+        const form = document.getElementById("form-1");
+
+        // block the form until the user completes all fields
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+        });
+        
+        // check first name
+        const firstName = document.getElementById("firstName");
+        const errorFirstName = document.getElementById("error-firstName");
+
+        firstName.addEventListener("keyup", function(event) {
+            const regexFirstName = /^[a-zA-Z-]+$/;
+            if (regexFirstName.test(event.target.value) == false) {
+                errorFirstName.innerHTML = "Prenumele trebuie sa contina litere mari si mici!";
+            } else {
+                errorFirstName.innerHTML = "";
+            }
+        });
+
+        // check last name
+        const lastName = document.getElementById("lastName");
+        const errorLastName = document.getElementById("error-lastName");
+        
+        lastName.addEventListener("keyup", function(event) {
+            const regexLastName = /^[a-zA-Z-]+$/;
+            if (regexLastName.test(event.target.value) == false) {
+                errorLastName.innerHTML = "Numele trebuie sa contina litere mari si mici!";
+            } else {
+                errorLastName.innerHTML = "";
+            }
+        });
+
+        // check email
+        const email = document.getElementById("email");
+        const errorEmail = document.getElementById("error-email");
+       
+        email.addEventListener("keyup", function(event) {
+            const regexEmail = /^[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+([a-zA-Z0-9-]*)\.)+[com, ro]+$/;
+            console.log(regexEmail.test(event.target.value));
+            if (regexEmail.test(event.target.value) == false) {
+                errorEmail.innerHTML = "Mailul introdus nu este valid";
+            }
+
+            if (regexEmail.test(event.target.value)) {
+                errorEmail.innerHTML = "";
+            }
+        });
+
+       // see password
+       const i = document.getElementById("pass-status");
+       i.addEventListener("click", function(event) {
+            var password = document.getElementById('password');
+            if (password.type == 'password'){
+                password.type='text';
+                i.className='fa fa-eye-slash';
+    
+                }
+                else{
+                password.type='password';
+                i.className='fa fa-eye';
+            }
+       });
+
+        // check password
+
+        const errorPassword = document.getElementById("error-password");
+        const password = document.getElementById("password");
+
+        password.addEventListener("keyup", function(event) {
+            const regexPassword = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*(){}~?-])[a-zA-Z0-9!@#$%^&*(){}~?-]{8,}/;
+            console.log(regexPassword.test(event.target.value));
+            if (regexPassword.test(event.target.value) == false) {
+                errorPassword.innerHTML = "Parola Slaba.<br> Introdu litere mari, mici, cifre, simboluri.<br> Parola trebuie sa aiba minim 8 caractere";
+            }
+
+            if (regexPassword.test(event.target.value)) {
+                errorPassword.innerHTML = "Parola Puternica";
+                errorPassword.style.color = "green";
+                
+            }
+        });
+
+        // check conditions for submitting
+        
+
+    };
+
+    validateForm();
 };
 createRegisterPage();
